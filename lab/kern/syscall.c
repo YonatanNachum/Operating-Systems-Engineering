78827b869527d393042ccbfc894eddc5d53b9393
yonatan360 <yonatan360@gmail.com>
2022-04-20 15:51:34 +0300

    Lab3: Part B: Page Faults, Breakpoints Exceptions, and System Calls
    Signed-off-by: yonatan360 <yonatan360@gmail.com>

--- lab/kern/syscall.c
+++ lab/kern/syscall.c
@@ -21,7 +21,7 @@ sys_cputs(const char *s, size_t len)
 	// Destroy the environment if not.
 
 	// LAB 3: Your code here.
-
+	user_mem_assert(curenv, (void *)s, len, 0);
 	// Print the string supplied by the user.
 	cprintf("%.*s", len, s);
 }
@@ -70,11 +70,26 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 	// Return any appropriate return value.
 	// LAB 3: Your code here.
 
-	panic("syscall not implemented");
-
 	switch (syscallno) {
+	case SYS_cputs:
+		sys_cputs((const char *)a1, a2);
+		break;
+	
+	case SYS_cgetc:
+		return sys_cgetc();
+		break;
+
+	case SYS_getenvid:
+		return sys_getenvid();
+		break;
+
+	case SYS_env_destroy:
+		return sys_env_destroy(a1);
+		break;
+	
 	default:
-		return -E_NO_SYS;
+		return -E_INVAL;
 	}
+	return 0;
 }
 
